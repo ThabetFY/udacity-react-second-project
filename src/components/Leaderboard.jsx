0e904596859cb9f3587e64ee09115a12a6649d9e
Thabet Alyahya <ThabetFy@outlook.com>
2024-01-24 21:34:04 +0300

    style: Add styling to Leaderboard component

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,46 +1,42 @@
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
-const Leaderboard = ({ users }) => {
-  console.log(users);
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Users</th>
-          <th>Answered</th>
-          <th>Created</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user, index) => (
-          <tr key={index}>
-            <td>{user.name}</td>
-            <td>{Object.keys(user.answers).length}</td>
-            <td>{user.questions.length}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-const mapStateToProps = ({ users }) => {
+const Leaderboard = () => {
+  const users = useSelector((state) => state.users);
   const sortedUsers = Object.values(users).sort(
     (a, b) =>
       b.questions.length +
       Object.keys(b.answers).length -
-      (a.questions.length + Object.keys(a.answers).length)
+      (a.questions.length + Object.keys(a.answers).length),
   );
-  return {
-    users: sortedUsers,
-  };
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Users</TableHead>
+          <TableHead>Answered</TableHead>
+          <TableHead>Created</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {sortedUsers.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell className="font-medium">{user.name}</TableCell>
+            <TableCell>{Object.keys(user.answers).length}</TableCell>
+            <TableCell>{user.questions.length}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
-Leaderboard.propTypes = {
-  users: PropTypes.array,
-};
-
-const connectedLeaderboard = connect(mapStateToProps)(Leaderboard);
-
-export default connectedLeaderboard;
+export default Leaderboard;
